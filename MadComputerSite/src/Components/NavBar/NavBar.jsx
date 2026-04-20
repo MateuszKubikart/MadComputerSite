5d901b8298250b43e4b19122ef62919b1c8cb9d4
Mateusz Kubik <mateuszkubikcr@interia.eu>
2026-04-20 17:27:35 +0200

    responzywnosc v2

--- MadComputerSite/src/Components/NavBar/NavBar.jsx
+++ MadComputerSite/src/Components/NavBar/NavBar.jsx
@@ -4,14 +4,78 @@ import "./NavBar.css";
 
 function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 1075);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 0);
     };
+
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 1075);
+      if (window.innerWidth > 1075) {
+        setIsMenuOpen(false);
+      }
+    };
+
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navItems = (
+    <ul className={`NavBarUl ${isMenuOpen ? "mobile-open" : ""}`}>
+      <li className="NavBarLi">
+        <div>
+          <a>Serwis</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>Fotowoltaika</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>Kasy Fiskalne</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>Usługi</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>Cennik</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>DPD Pickup</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>O nas</a>
+        </div>
+      </li>
+      <li className="NavBarLi">
+        <div>
+          <a>Kontakt</a>
+        </div>
+      </li>
+    </ul>
+  );
 
   return (
     <header>
@@ -19,50 +83,27 @@ function NavBar() {
         <a href="/" className="NavLogo">
           <img src={logo} alt="Logo Sklepu" width="100px" />
         </a>
-        <nav className="NavBar">
-          <ul className="NavBarUl">
-            <li className="NavBarLi">
-              <div>
-                <a>Serwis</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>Fotowoltaika</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>Kasy Fiskalne</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>Usługi</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>Cennik</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>DPD Pickup</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>O nas</a>
-              </div>
-            </li>
-            <li className="NavBarLi">
-              <div>
-                <a>Kontakt</a>
-              </div>
-            </li>
-          </ul>
-        </nav>
+
+        {isMobileView ? (
+          <div className="MobileMenuContainer">
+            <button className="MenuToggle" onClick={toggleMenu}>
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="12" cy="5" r="2" fill="#252526" />
+                <circle cx="12" cy="12" r="2" fill="#252526" />
+                <circle cx="12" cy="19" r="2" fill="#252526" />
+              </svg>
+            </button>
+            {isMenuOpen && <nav className="NavBarMobile">{navItems}</nav>}
+          </div>
+        ) : (
+          <nav className="NavBar">{navItems}</nav>
+        )}
       </div>
     </header>
   );
